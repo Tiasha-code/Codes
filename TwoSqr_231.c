eb500dafcb87b77d8b50f62e2aa50e0bec8301b0
Tiasha-code <tiashaganguly2005@gmail.com>
2025-08-09 23:49:37 +0530

    commit

---
+++ TwoSqr_231.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+bool isPowerOfTwo(int n) {
+    // Must be positive and have only one bit set
+    return n > 0 && (n & (n - 1)) == 0;
+}
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if (isPowerOfTwo(n))
+
+        printf("true\n");
+    else
+        printf("false\n");
+
+    return 0;
+}
